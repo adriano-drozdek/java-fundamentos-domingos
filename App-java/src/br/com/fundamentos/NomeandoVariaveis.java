a57b24bf45a31507f3076a8ef0cd4e18203b9c25
Adriano Drozdek <61703@PROWAY.TREINA>
2020-09-05 17:46:32 -0300

    5 Tipos de variaveis que não podem ser utilizadas

---
+++ App-java/src/br/com/fundamentos/NomeandoVariaveis.java
@@ -0,0 +1,27 @@
+package br.com.fundamentos;
+
+public class NomeandoVariaveis {
+   public static void main(String[] args) {
+	// valido separado por underline_ 
+	String variavel_separada_por_underline;
+	
+	// padrão camelCase
+	String variavelSeparadaPorCamelCase;
+	
+	// valido porem estranho
+	int _numero = 1;
+	
+	System.out.println(_numero);
+	
+	// valido porem mais estranho ainda! parec PHP
+	int $outroNumero = 1;
+	System.out.println($outroNumero);
+	
+	// Não pode começar com caracteres especiais
+	String 1um;
+	
+	int @arroba;
+	
+	String *texto 
+}
+}
